2e9259b0192654cf1af1a36706b2c001c591c8cc
ashishagrawalcode <agrawalashish815@gmail.com>
2026-06-05 22:59:56 +0545

    Server error fixes

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@ledzer/ui", "@ledzer/database"],
+  outputFileTracing: true, // Ensure this is true
 };
 
 export default nextConfig;
