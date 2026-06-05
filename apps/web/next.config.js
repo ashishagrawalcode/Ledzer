3e65d2dd63b8ed1b1d70a4fb69b4682d90ad7c1a
ashishagrawalcode <agrawalashish815@gmail.com>
2026-06-05 23:16:22 +0545

    Server error fixes

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@ledzer/ui", "@ledzer/database"],
-  outputFileTracing: true, // Ensure this is true
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**/*': [
+        './node_modules/.prisma/client/libquery_engine-rhel-openssl-3.0.x.so.node'
+      ],
+    },
+  },
 };
 
 export default nextConfig;
