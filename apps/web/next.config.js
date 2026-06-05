df57f92c9b75b7ff295a2c749b77981e8947cef4
ashishagrawalcode <agrawalashish815@gmail.com>
2026-06-05 23:20:49 +0545

    package fixes

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,9 +3,7 @@ const nextConfig = {
   transpilePackages: ["@ledzer/ui", "@ledzer/database"],
   experimental: {
     outputFileTracingIncludes: {
-      '/api/**/*': [
-        './node_modules/.prisma/client/libquery_engine-rhel-openssl-3.0.x.so.node'
-      ],
+      "/api/**/*": ["./node_modules/.prisma/client/*.node"],
     },
   },
 };
